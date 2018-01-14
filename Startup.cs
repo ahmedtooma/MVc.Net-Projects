6edc496fd3d1ae13f7f870e0c45bf64fdc355125
ahmedtooma <30286017+ahmedtooma@users.noreply.github.com>
2018-01-14 15:54:05 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobFindsDemo.Startup))]
+namespace JobFindsDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
