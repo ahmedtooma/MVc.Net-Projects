3b1d8ca934cef01b8054d5021a788b5697e3c649
ahmedtooma <30286017+ahmedtooma@users.noreply.github.com>
2018-01-22 14:58:08 +0200

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.Owin;
+﻿using jobaffair.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(JobFindsDemo.Startup))]
-namespace JobFindsDemo
+[assembly: OwinStartupAttribute(typeof(jobaffair.Startup))]
+namespace jobaffair
 {
     public partial class Startup
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            DefaultRolesAndUsers();
         }
+
+        public void DefaultRolesAndUsers()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            IdentityRole role = new IdentityRole();
+            if (!roleManager.RoleExists("Admins"))
+            {
+
+                role.Name = "Admins";
+                roleManager.Create(role);
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "khalid";
+                user.Email = "khalid@hotmail.com";
+                var check = userManager.Create(user, "asD@123");
+                if (check.Succeeded)
+                {
+
+                    userManager.AddToRole(user.Id, "Admins");
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
     }
 }
